3c43537357769c9b6fd22a7a940ccb37d60c1ca1
emacc99 <lalitoceja@gmail.com>
2021-10-29 16:30:43 -0500

    made the register the default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from "./login-screen/login-screen.component";
 import { RegisterScreenComponent } from "./register-screen/register-screen.component";
 const appRoutes = [
   // { path: "", component: AppComponent, pathMatch: "full" },
-  {path: "", redirectTo:"/login", pathMatch: "full"},
+  {path: "", redirectTo:"/register", pathMatch: "full"},
   { path: "login", component: LoginComponent, pathMatch: "full" },
   { path: "register", component: RegisterScreenComponent, pathMatch: "full" }
 ];
