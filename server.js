a330f88266dd461499c3d6df177c5a6e7aa67064
Javier <javojavojavojavo@gmail.com>
2021-11-07 20:24:24 -0600

    prod config

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
-app.use(express.static('./dist/angular-login'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-login/'}),
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
 );
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
